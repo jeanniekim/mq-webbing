1f8ba15c2452ffa1894809fdb4833b0456a8d2ec
Jeannie Kim <88344337+jeanniekim@users.noreply.github.com>
2022-01-20 13:59:30 -0800

    Wrote first try
    convert to line by line?

--- LinkFixer.java
+++ LinkFixer.java
@@ -16,19 +16,84 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.Arrays; 
 
 public class LinkFixer {
     public static void main(String[] args) throws IOException{
         //Path fileName = Path.of(args[0]);
+        
         // Getting contents of file
-        Path fileName = Path.of("toFix.html");
+        String file = "toFix.html";
+        Path fileName = Path.of(file);
 	    List<String> contents = Files.readAllLines(fileName);
-        System.out.println(contents);
+        String rawContents = Files.readString(fileName);
+        ArrayList<String> relevantLines = new ArrayList<>();
+
+        for (String line: contents){
+            if (line.length() > 1){
+               if (line.charAt(1) == 'p'){
+                relevantLines.add(line);
+                } 
+            }
+        }
+    
+        String[][] bigMaster = processLine(relevantLines)
+
+        // Write a new file
+
+        String fixedContents = rawContents;
+
+        for (String[] replacePair : bigMaster){
+            fixedContents.replace(replacePair[0], replacePair[1]);
+        }
+
+        System.out.println(fixedContents);
     }
     
-    // public static ArrayList<String> getLinks(String[] contents) {
-        
-    // }
+    public static String[][] processLine(ArrayList<String> lines) {
+       
+        String[][] toReturn = new String[lines.size()][2];
+
+        for (int i = 0; i < lines.size(); i++){ // for each line
+            String line = lines.get(i);
+
+            // Finding + processing name
+            
+            int nameStartInd = 0;
+            int nameEndInd = line.length()-8; // </a></p>
+            
+            // traversing backwards from EndIndex
+            for (int k = nameEndInd; k > 0; k--){
+                // starts after first >
+                if (line.charAt(k) == '>'){
+                    nameStartInd = k+1;
+                    break;
+                }
+            }
+            String name = line.substring(nameStartInd, nameEndInd);
+            toReturn[i][0] = toNewLink(name);
+
+            // Finding + processing (old, maybe wrong) link
+            int linkStartInd = line.indexOf("data-id=")+8;
+            int linkEndInd = line.length();
+            for (int j = linkStartInd; j < line.length(); j++){
+                if (line.charAt(j) == '>'){
+                    linkEndInd = j+1;
+                    break;
+                }
+            }
+            String oldLink = line.substring(linkStartInd, linkEndInd);
+            toReturn[i][1] = oldLink;
+        }
+
+        return toReturn;
+    }
+
+    public static String toNewLink(String name){
+        String[] nameSplit = name.toLowerCase().split(" ");
+        String username = nameSplit[0].charAt(0) + nameSplit[1];
+        return "https://themq.org/author/" + username + "/";
+    }
 
 }
